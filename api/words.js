056eff66da2aa5d4c403e8849915086985d96e18
miniaturity <miniaturity2.o@gmail.com>
2025-11-19 22:10:34 -0500

    fix: api

--- api/words.js
+++ api/words.js
@@ -2,26 +2,7 @@ const { google } = require("googleapis");
 
 module.exports = async function handler(req, res) {
   try {
-    // Debug logging
-    console.log('GOOGLE_CLIENT_EMAIL exists:', !!process.env.GOOGLE_CLIENT_EMAIL);
-    console.log('PRIVATE_KEY exists:', !!process.env.PRIVATE_KEY);
-    console.log('PRIVATE_KEY length:', process.env.PRIVATE_KEY?.length);
-    console.log('PRIVATE_KEY first 50 chars:', process.env.PRIVATE_KEY?.substring(0, 50));
-    
-    if (!process.env.PRIVATE_KEY || !process.env.GOOGLE_CLIENT_EMAIL) {
-      throw new Error('Missing required environment variables');
-    }
-
-    let credentials;
-    try {
-      credentials = JSON.parse(process.env.PRIVATE_KEY);
-      console.log('Parsed as JSON, has private_key:', !!credentials.private_key);
-    } catch {
-      credentials = {
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-      };
-    }
+    const credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON);
 
     const auth = new google.auth.GoogleAuth({
       credentials: credentials,
@@ -45,4 +26,4 @@ module.exports = async function handler(req, res) {
       details: err.message 
     });
   }
-};
+}
